f53b33dde9dd1afc5757b0073cb2f73b084a6e43
Wilhelm Simus <wilhelm.simus@gmail.com>
2024-12-29 17:19:27 +0100

    Day 6 done

--- 2024/solveDay06.py
+++ 2024/solveDay06.py
@@ -1,3 +1,5 @@
+import time
+
 test = False
 inputFile = ''
 outputFile = ''
@@ -25,26 +27,27 @@ for line in input:
 dimY = len(map)
 dimX = len(map[0])
 obstacles = []
-pos = []
+start = []
 dir = 'none'
 for y in range(dimY):
     for x in range(dimX):
         if (map[y][x] == '#'):
             obstacles.append([x, y])
         elif (map[y][x] == '^'):
-            pos = [x, y, 'up'] # this way even multiple guards can be monitored
+            start = [x, y, 'up'] 
         elif (map[y][x] == 'v'):
-            pos = [x, y, 'down'] # this way even multiple guards can be monitored
+            start = [x, y, 'down'] 
         elif (map[y][x] == '<'):
-            pos = [x, y, 'left'] # this way even multiple guards can be monitored
+            start = [x, y, 'left'] 
         elif (map[y][x] == '>'):
-            pos = [x, y, 'right'] # this way even multiple guards can be monitored        
+            start = [x, y, 'right']      
 # 3. walk path
 def checkObstacle(x, y, obstLst):
     for obst in obstLst:
         if([x, y] == obst):
             return True
     return False
+pos = start.copy()
 path = [pos[:-1]]
 wardenLeft = False
 while not wardenLeft:
@@ -99,8 +102,92 @@ def countDistinct(list):
 countPos = countDistinct(path)
 
 out = countPos
-output.write('Solution: ')
-output.write(str(out) + '\n')
+output.write('The warden visits ')
+output.write(str(out) + ' distinct positions. \n')
+
+def hasVisited(pos, path):
+    #print('Looking for position ' + str(pos) + ' in path ' + str(path))
+    for step in path:
+        if step == pos:
+            #print(str(step) + ' is the same as ' + str(pos))
+            return True
+    return False
+
+def renderTime(time):
+    if time < 60:
+        return str(time) + ' sec'
+    elif time < 3600:
+        minutes = int(time/60)
+        seconds = time - (minutes * 60)
+        return str(minutes) + ' min ' + str(seconds) + ' sec'
+    else:
+        hours = int(time/3600)
+        minutes = int((time-(hours*3600))/60)
+        seconds = time - (hours*3600 + minutes * 60)
+        return str(hours) + ' h ' + str(minutes) + ' min ' + str(seconds) + ' sec'
+
+startTime = time.time()
+progress = 0
+countLoop = 0
+for y in range(dimY):
+    for x in range(dimX):
+        progress += 1/(dimX*dimY)
+        elapsedTime = time.time() - startTime
+        estimatedTime = elapsedTime/progress
+        leftTime = estimatedTime - elapsedTime
+        print('Progress: ' + str(int(100*progress)/100) + '%, Time elapsed: ' + renderTime(int(elapsedTime)) + ', Time left (estimate): ' + renderTime(int(leftTime)), end='\r')
+        nu = [[x, y]]
+        nuObst = obstacles + nu
+        pos = start.copy()
+        #print (pos)
+        path = [pos.copy()] # this time the direction the warden is facing is important during the path
+        wardenLeft = False
+        loopEncountered = False
+        while not (wardenLeft or loopEncountered):
+            # check next step
+            obstacle = False
+            if (pos[2] == 'up'):
+                obstacle = checkObstacle(pos[0], pos[1]-1, nuObst)
+            elif (pos[2] == 'down'):
+                obstacle = checkObstacle(pos[0], pos[1]+1, nuObst)
+            elif (pos[2] == 'left'):
+                obstacle = checkObstacle(pos[0]-1, pos[1], nuObst)
+            else:
+                obstacle = checkObstacle(pos[0]+1, pos[1], nuObst)
+            # turn right if an obstacle is encountered
+            if obstacle:
+                if (pos[2] == 'up'):
+                    pos[2] = 'right'
+                elif (pos[2] == 'right'):
+                    pos[2] = 'down'
+                elif (pos[2] == 'down'):
+                    pos[2] = 'left'
+                elif (pos[2] == 'left'):
+                    pos[2] = 'up'
+            # take next step if no obstacle is encountered
+            else:
+                if (pos[2] == 'up'):
+                    pos[1] -= 1
+                elif (pos[2] == 'right'):
+                    pos[0] += 1
+                elif (pos[2] == 'down'):
+                    pos[1] += 1
+                elif (pos[2] == 'left'):
+                    pos[0] -= 1
+            # check if left
+            if ((pos[0] < 0) or (pos[1] < 0) or (pos[0] >= dimX) or (pos[1] >= dimY)):
+                wardenLeft = True
+                #print ('Warden left.')
+            # check if loop
+            elif hasVisited(pos, path):
+                loopEncountered = True
+                countLoop += 1
+                #print('Loop for introducing new obstacle at ' + str(nu[0]))
+            else:
+                path.append(pos.copy())
+
+out = countLoop
+output.write(str(out) + ' loops can be introduced using a new obstacle. \n')
 
 input.close()
 output.close()
